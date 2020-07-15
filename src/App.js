14647cd41bcc0137aef35de20777375a68c76870
Coby Forrester <cobyforrester@Cobys-MacBook-Pro.local>
2020-07-15 15:14:32 -0700

    axios working on backend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
+
+const loadProjects = () => {
+  return axios.get('http://127.0.0.1:8000/api/projects/')
+  .then((response) => {
+    return response
+  })
+  .catch(error => {
+    return Promise.reject(error);
+  });
+}
+
 function App() {
+  const [projects, setProjects] = useState([])
+  useEffect(() => {
+    loadProjects().then(response =>{
+      if(response.status === 200){
+        setProjects(response.data);
+      }
+    }).catch(error =>{
+      console.log(error)
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {projects.map((project, index) => {
+            return <li>{project.title}</li>
+          })}
         </p>
         <a
           className="App-link"
