5ed5995a5f5136b5ad0c6038747402ca5e66cc63
Coby Forrester <cobyforrester@Cobys-MacBook-Pro.local>
2020-07-16 20:35:32 -0700

    Member Btn WIP

--- src/projects/components.js
+++ src/projects/components.js
@@ -4,11 +4,11 @@ import {loadProjects} from '../lookup'
 export const ProjectsList = (props) => {
 const [projects, setProjects] = useState([])
 useEffect(() => {
-    loadProjects().then(response =>{
+    loadProjects().then(response => {
     if(response.status === 200){
         setProjects(response.data);
     }
-    }).catch(error =>{
+    }).catch(error => {
     console.log(error)
     });
 }, []);
@@ -18,20 +18,57 @@ return projects.map((item, index) => {
 }
 
 
-export const ActionBtn = (props) => {
-    const {action, className} = props
-    return <button className={className}>{action}</button>
-  }
+export const ActionMemberBtn = (props) => {
+    //const {action, className} = props
+    const description = {
+        addMember: 'Add Username',
+        removeMember: 'Remove Username',
+        edit: 'Edit Member List',
+        cancel: 'Cancel',
+    }
+    const className = {
+        addBtnClass: 'btn btn-success btn-sm',
+        removeBtnClass: 'btn btn-danger btn-sm',
+        cancelBtnClass: 'btn btn-light btn-sm',
+        neutralBtnClass: 'btn btn-info btn-sm',
+    }
+
+    const [clickAddRemove, setClickAddRemove] = useState(false)
+    if(!clickAddRemove) {
+    return <div>
+        <form>
+            <button className={className.neutralBtnClass} 
+            onClick={() => {
+                setClickAddRemove(!clickAddRemove);
+                
+                }}>
+                {description.edit}</button>
+            <input type="text" name="" /> 
+        </form>
+        </div >
+    }
+    else {
+        return <div>
+        <form>
+            <button className={className.removeBtnClass} 
+            onClick={() => {
+                setClickAddRemove(!clickAddRemove);
+                
+                }}>
+                {description.edit}</button>
+            <input type="text" name="" /> 
+        </form>
+        </div >
+
+    }
+}
   
 export const Project = (props) => {
 const {project} = props
-const addBtnClass = 'btn btn-success btn-sm'
-const RmvBtnClass = 'btn btn-danger btn-sm'
 return <div className='col-10 mx-auto col-md-6'>
     <p>{project.id} - {project.title}</p>
     <div className='btn btn-group'>
-    <ActionBtn action={'Add To Project'} className={addBtnClass} />
-    <ActionBtn action={'Remove From Project'} className={RmvBtnClass} />
+    <ActionMemberBtn />
     </div>
 </div>
 }
