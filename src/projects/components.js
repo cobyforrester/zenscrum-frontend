448c00c346281ff4411a6e338bb80045e4481c94
Coby Forrester <cobyforrester@Cobys-MacBook-Pro.local>
2020-07-17 19:44:21 -0700

    Project component WIP

--- src/projects/components.js
+++ src/projects/components.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {loadProjects} from '../lookup'
+import React, { useEffect, useState } from 'react';
+import {loadProjects} from '../lookup';
   
 export const ProjectsList = (props) => {
 const [projects, setProjects] = useState([])
@@ -14,61 +14,75 @@ useEffect(() => {
 }, []);
 return projects.map((item, index) => {
     return <Project project = {item} key={`${index}-item.id`} />
+
 })
 }
 
-
 export const ActionMemberBtn = (props) => {
-    //const {action, className} = props
-    const description = {
-        addMember: 'Add Username',
-        removeMember: 'Remove Username',
-        edit: 'Edit Member List',
-        cancel: 'Cancel',
-    }
-    const className = {
-        addBtnClass: 'btn btn-success btn-sm',
-        removeBtnClass: 'btn btn-danger btn-sm',
-        cancelBtnClass: 'btn btn-light btn-sm',
-        neutralBtnClass: 'btn btn-info btn-sm',
-    }
+    const {action, className, clickedFunc} = props
+    const description = action.description
 
-    const [clickAddRemove, setClickAddRemove] = useState(false)
-    if(!clickAddRemove) {
+    //const [clickAddRemove, setClickAddRemove] = useState(false)
     return <div>
-        <form>
-            <button className={className.neutralBtnClass} 
-            onClick={() => {
-                setClickAddRemove(!clickAddRemove);
-                
-                }}>
-                {description.edit}</button>
-            <input type="text" name="" /> 
-        </form>
-        </div >
-    }
-    else {
-        return <div>
-        <form>
-            <button className={className.removeBtnClass} 
-            onClick={() => {
-                setClickAddRemove(!clickAddRemove);
-                
-                }}>
-                {description.edit}</button>
-            <input type="text" name="" /> 
-        </form>
-        </div >
+        <button onClick={() => {
+        clickedFunc();
+    }} className={className} >{description}</button>
+        </div>
 
-    }
-}
+  }
   
 export const Project = (props) => {
-const {project} = props
+const {project} = props;
+
+// INITIAL VALUES FOR STATE, CLICKED = FALSE
+let tmpState = {
+    clicked: false,
+    addBtnClass: 'btn btn-success btn-sm d-none',
+    rmvBtnClass: 'btn btn-danger btn-sm d-none',
+    edtBtnClass: 'btn btn-info btn-sm',
+    memberFormState: 'd-none',
+}
+const [state, setState] = useState(tmpState) //this is just whether add or not is seen
+const clickedFunc = () => {
+    if (state.clicked) {
+        setState({...state, 
+            clicked: false,
+            addBtnClass: 'btn btn-success btn-sm d-none',
+            rmvBtnClass: 'btn btn-danger btn-sm d-none', 
+            edtBtnClass: 'btn btn-info btn-sm',
+            memberFormState: 'd-none',
+        })
+    }
+    else {
+        setState({...state, 
+            clicked: true,
+            addBtnClass: 'btn btn-success btn-sm',
+            rmvBtnClass: 'btn btn-danger btn-sm',
+            edtBtnClass: 'btn btn-info btn-sm d-none', 
+            memberFormState: '',
+        })
+    }
+}
+useEffect(() => {}, [state]);
+
 return <div className='col-10 mx-auto col-md-6'>
-    <p>{project.id} - {project.title}</p>
-    <div className='btn btn-group'>
-    <ActionMemberBtn />
-    </div>
+
+    <div className="card border mb-4 mt-4">
+        <div className="card-body">
+        <h5 className="card-title">Project: {project.title} - {project.id}</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <form className={state.memberFormState}>
+            <input type="text"  placeholder="Enter Username" />
+        </form>
+
+        <div className='btn btn-group'>
+            <ActionMemberBtn clickedFunc={clickedFunc} action={{type: 'add', description: 'Add To Project'}} className={state.addBtnClass} />
+            <ActionMemberBtn clickedFunc={clickedFunc} action={{type: 'remove', description: 'Remove From Project'}} className={state.rmvBtnClass} />
+            <ActionMemberBtn clickedFunc={clickedFunc} action={{type: 'edit', description: 'Add/Remove Members'}} className={state.edtBtnClass} />
+        </div>
+        
+  </div>
+</div>
+
 </div>
 }
