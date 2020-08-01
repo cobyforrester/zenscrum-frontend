a34acee839d6e36430fb89b07dca7ce7ba746940
Coby Forrester <cobyforrester@Cobys-MacBook-Pro.local>
2020-07-31 21:28:55 -0700

    https requests

--- src/lookup/components.js
+++ src/lookup/components.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const lookup = (method, endpoint, data, headers) => {
   return axios({
     method: method,
-    url: `http://scrummy-backend.herokuapp.com/api/${endpoint}`,
+    url: `https://scrummy-backend.herokuapp.com/api/${endpoint}`,
     data: data,
     headers: headers,
   })
